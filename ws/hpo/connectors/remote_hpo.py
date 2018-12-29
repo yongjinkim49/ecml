714f05946dc5e2c22bb8a138766919dfc608d3bd
Hyunghun Cho <hyunghunny@gmail.com>
2018-12-29 15:14:23 +0900

    change validation logic

--- ws/hpo/connectors/remote_hpo.py
+++ ws/hpo/connectors/remote_hpo.py
@@ -13,9 +13,12 @@ from ws.hpo.connectors.remote_job import RemoteJobConnector
 class RemoteOptimizerConnector(RemoteJobConnector):
     
     def __init__(self, ip_addr, port, cred, **kwargs):
-        url = "http://{}:{}".format(ip_addr, port)
+        
         self.ip_addr = ip_addr
         self.port = port
+        
+        url = "http://{}:{}".format(ip_addr, port)
+        
         super(RemoteOptimizerConnector, self).__init__(url, cred, **kwargs)
 
     def validate(self):
@@ -23,10 +26,7 @@ class RemoteOptimizerConnector(RemoteJobConnector):
             profile = self.get_profile()
             if profile and "spec" in profile and "job_type" in profile["spec"]:
                 if profile["spec"]["job_type"] == "HPO_runner":
-                    config = self.get_config()
-                    if config and "arms" in config and len(config["arms"]) > 0:
-                        # TODO: parameter validation process                  
-                        return True       
+                    return True       
 
         except Exception as ex:
             warn("Validation failed: {}".format(ex))
