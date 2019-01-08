15dbe15be23366f1e2bac8777ff6485d99c479da
Hyunghun Cho <hyunghunny@gmail.com>
2019-01-08 13:05:25 +0900

    update txpb run script

--- txpb.sh
+++ txpb.sh
@@ -8,7 +8,7 @@
 #python hpo_runner.py -m RF -s PI -et 1d data2 100
 #python hpo_runner.py -m RF -s UCB -et 1d data2 100
 
-python hpo_runner.py -rc gp-hedge3.json -m ADA -s BO-HEDGE -et 1d data2 100
+#python hpo_runner.py -rc gp-hedge3.json -m ADA -s BO-HEDGE -et 1d data2 100
 
 # sequential diversification
 #python hpo_runner.py -m DIV -s RANDOM -et 1d data2 100
@@ -17,7 +17,7 @@ python hpo_runner.py -rc gp-hedge3.json -m ADA -s BO-HEDGE -et 1d data2 100
 #python hpo_runner.py -rc arms-pure.json -m DIV -s SEQ -et 1d data2 100
 
 # parallel diversification
-python hpo_runner.py -rc p6gp.json -m BATCH -s SYNC -et 4h data2 100
+#python hpo_runner.py -rc p6gp.json -m BATCH -s SYNC -et 4h data2 100
 python hpo_runner.py -rc p6gp.json -m BATCH -s ASYNC -et 4h data2 100
 python hpo_runner.py -rc p6gp-nm.json -m BATCH -s ASYNC -et 4h data2 100
 #python hpo_runner.py -m BATCH -s ASYNC -et 4h data2 100
