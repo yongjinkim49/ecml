c83495b46c4284dfd851054f28d3d6cc7516a0e9
Hyunghun Cho <hyunghunny@gmail.com>
2019-01-09 11:39:44 +0900

    update experiment script

--- titan.sh
+++ titan.sh
@@ -15,9 +15,9 @@
 #python hpo_runner.py -rc arms-log.json -m DIV -s SEQ -et 1d data30 100
 #python hpo_runner.py -rc arms-pure.json -m DIV -s SEQ -et 1d data30 100
 # Parallel BO
-python hpo_runner.py -rc p6gp.json -m BATCH -s SYNC -et 4h data30 100
-python hpo_runner.py -rc p6gp.json -m BATCH -s ASYNC -et 4h data30 100
-python hpo_runner.py -rc p6gp-nm.json -m BATCH -s ASYNC -et 4h data30 100
+#python hpo_runner.py -rc p6gp.json -m BATCH -s SYNC -et 4h data30 100
+#python hpo_runner.py -rc p6gp.json -m BATCH -s ASYNC -et 4h data30 100
+#python hpo_runner.py -rc p6gp-nm.json -m BATCH -s ASYNC -et 4h data30 100
 python hpo_runner.py -rc p6rf.json -m BATCH -s ASYNC -et 4h data30 100
 # Parallel diversification
 python hpo_runner.py -rc no_failover.json -m BATCH -s ASYNC -et 4h data30 100
