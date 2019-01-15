22f18caecc9c995236e74d233cfa0becbe1f39a6
Hyunghun Cho <hyunghunny@gmail.com>
2019-01-15 15:07:57 +0900

    add data30 median ETR test

--- tia.sh
+++ tia.sh
@@ -23,8 +23,8 @@
 #python hpo_runner.py -rc no_failover.json -m BATCH -s ASYNC -et 4h data10 100
 #python hpo_runner.py -m BATCH -s ASYNC -et 4h data10 100
 # Sequential diversification with ETR
-#python hpo_runner.py -m DIV -s SEQ -et 1d -etr VizMedian data10 100
+python hpo_runner.py -m DIV -s SEQ -et 1d -etr VizMedian data30 100
 #python hpo_runner.py -m DIV -s SEQ -et 1d -etr Interval data10 100
 #python hpo_runner.py -m DIV -s SEQ -et 1d -etr Knock data10 100
 #python hpo_runner.py -m DIV -s SEQ -et 1d -etr IntervalKnock data10 100
-python hpo_runner.py -m DIV -s SEQ -et 1d -etr KickStarter data30 100
+#python hpo_runner.py -m DIV -s SEQ -et 1d -etr KickStarter data30 100
