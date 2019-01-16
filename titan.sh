76ffab569118558a6a9ce8bdb8535b709435a5f1
Hyunghun Cho <hyunghunny@gmail.com>
2019-01-16 20:00:11 +0900

    IPO on data2

--- titan.sh
+++ titan.sh
@@ -28,3 +28,4 @@
 #python hpo_runner.py -m DIV -s SEQ -et 1d -etr Knock data30 100
 #python hpo_runner.py -m DIV -s SEQ -et 1d -etr IntervalKnock data30 100
 #python hpo_runner.py -m DIV -s SEQ -et 1d -etr IntervalPentaOpt data30 100
+python hpo_runner.py -m DIV -s SEQ -et 1d -etr IntervalPentaOpt data2 100
