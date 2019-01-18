ce1ebafdffd7ed2cf836ad4e4479e69f0e8590c0
Hyunghun Cho <hyunghunny@gmail.com>
2019-01-18 17:36:54 +0900

    fix typo

--- my_laptop.sh
+++ my_laptop.sh
@@ -1,10 +1,7 @@
-
 # Parallel diversification with ETR
-python hpo_runner.py -rc p6dic-etr.json -m BATCH -s ASYNC -et 30h data207 100
-python hpo_runner.py -rc p6dic-etr.json -m BATCH -s ASYNC -et 4h data10 100
-python hpo_runner.py -rc p6dic-etr.json -m BATCH -s ASYNC -et 4h data20 100
-python hpo_runner.py -rc p6dic-etr.json -m BATCH -s ASYNC -et 4h data2 100
-python hpo_runner.py -rc p6dic-etr.json -m BATCH -s ASYNC -et 4h data3 100
-python hpo_runner.py -rc p6dic-etr.json -m BATCH -s ASYNC -et 4h data30 100
-
-
+python hpo_runner.py -rc p6div-etr.json -m BATCH -s ASYNC -et 30h data207 100
+python hpo_runner.py -rc p6div-etr.json -m BATCH -s ASYNC -et 4h data10 100
+python hpo_runner.py -rc p6div-etr.json -m BATCH -s ASYNC -et 4h data20 100
+python hpo_runner.py -rc p6div-etr.json -m BATCH -s ASYNC -et 4h data2 100
+python hpo_runner.py -rc p6div-etr.json -m BATCH -s ASYNC -et 4h data3 100
+python hpo_runner.py -rc p6div-etr.json -m BATCH -s ASYNC -et 4h data30 100
