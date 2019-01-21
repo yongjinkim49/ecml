6b843073a8a91362f6d31a1f09ac67928720dd83
Hyunghun Cho <hyunghunny@gmail.com>
2019-01-21 12:33:24 +0900

    update run script

--- xcutes/etr_test_mid.sh
+++ xcutes/etr_test_mid.sh
@@ -3,5 +3,4 @@ python hpo_runner.py -m DIV -s SEQ -e GOAL -eg 0.9933 -et 1d -etr TetraTercet da
 python hpo_runner.py -m DIV -s SEQ -e GOAL -eg 0.784 -et 1d -etr PentaTercet data20 100
 python hpo_runner.py -m DIV -s SEQ -e GOAL -eg 0.784 -et 1d -etr DecaTercet data20 100
 python hpo_runner.py -m DIV -s SEQ -e GOAL -eg 0.784 -et 1d -etr TetraTercet data20 100
-python hpo_runner.py -m DIV -s SEQ -e GOAL -eg 0.9334 -et 5d -etr TetraTercet data207 100
 
