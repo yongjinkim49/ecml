3630f9936c6bd1f019a259bf26c09e1d0ff656ef
Hyunghun Cho <hyunghunny@gmail.com>
2019-01-22 14:28:10 +0900

    fix typo

--- xcutes/etr_test_hard.sh
+++ xcutes/etr_test_hard.sh
@@ -1,2 +1,1 @@
 python hpo_runner.py -m DIV -s SEQ -e GOAL -eg 0.9334 -et 1d -etr PentaTercet data207 100
-python hpo_runner.py -m DIV -s SEQ -e GOAL -eg 0.9334 -et 5d -etr TetraTercet data207 100
--- xcutes/titan.sh
+++ xcutes/titan.sh
@@ -30,4 +30,4 @@
 #python hpo_runner.py -m DIV -s SEQ -et 1d -etr IntervalPentaOpt data30 100
 #python hpo_runner.py -m DIV -s SEQ -et 1d -etr IntervalPentaOpt data2 100
 python hpo_runner.py -rc p6div-etr-w12h.json -m DIV -s SEQ -et 1d data30 100
-python hpo_runner.py -rc p6div-etr-w3d.json -m DIV -s SEQ -e -et 5d data207 100
+python hpo_runner.py -rc p6div-etr-w3d.json -m DIV -s SEQ -et 5d data207 100
