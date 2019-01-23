cf202e538b27925fc2423a1ec9a149e3b1907962
Hyunghun Cho <hyunghunny@gmail.com>
2019-01-23 15:56:29 +0900

    update for missing experiments

--- xcutes/tv4.sh
+++ xcutes/tv4.sh
@@ -1,5 +1,6 @@
 #python hpo_runner.py -rc p6div-etr-w12h.json -m DIV -s SEQ -et 1d data20 100
-python hpo_runner.py -rc p6gp_ucb.json -m BATCH -s ASYNC -et 4h data2 100
-python hpo_runner.py -rc p6gp_ucb.json -m BATCH -s ASYNC -et 4h data3 100
-python hpo_runner.py -rc p6gp_ucb.json -m BATCH -s ASYNC -et 4h data10 100
-python hpo_runner.py -rc p6gp_ucb.json -m BATCH -s ASYNC -et 4h data20 100
+#python hpo_runner.py -rc p6gp_ucb.json -m BATCH -s ASYNC -et 4h data2 100
+#python hpo_runner.py -rc p6gp_ucb.json -m BATCH -s ASYNC -et 4h data3 100
+#python hpo_runner.py -rc p6gp_ucb.json -m BATCH -s ASYNC -et 4h data10 100
+#python hpo_runner.py -rc p6gp_ucb.json -m BATCH -s ASYNC -et 4h data20 100
+python hpo_runner.py -rc p6gp_pi.json -m BATCH -s ASYNC -et 4h data30 100
