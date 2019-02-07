d5e907f78f05c34b0bdb2d12cc419d40e7a78fcc
Hyunghun Cho <hyunghunny@gmail.com>
2019-02-07 16:01:30 +0900

    update for baseline evaluation

--- run_hpo1.sh
+++ run_hpo1.sh
@@ -1,1 +1,1 @@
-python xcutes/train_benchmark.py 6001 2>&1 | tee hpo6001_log.txt
+python xcutes/train_baseline.py 6001 2>&1 | tee hpo6001_log.txt
--- xcutes/train_baseline.py
+++ xcutes/train_baseline.py
@@ -44,7 +44,7 @@ def test_run_main(surrogate, port, trials, duration):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('port', type=int, default=6000, help='Port number.')
-    parser.add_argument('-t', '--trials', type=int, default=1, help='number of trials.')
+    parser.add_argument('-t', '--trials', type=int, default=2, help='number of trials.')
     parser.add_argument('-d', '--duration', type=str, default="12h", help='The walltime to optimize.')
     args = parser.parse_args()
     
