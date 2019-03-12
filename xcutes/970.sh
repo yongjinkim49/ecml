ba801ac8771a69f97647d0a8b13eab9565f12603
Hyunghun Cho <hyunghunny@gmail.com>
2019-03-12 17:46:24 +0900

    run script update

--- xcutes/970.sh
+++ xcutes/970.sh
@@ -1,15 +1,15 @@
 # Parallel diversification
 # no log + no ETR
-python hpo_runner.py -rc p6div-no_log-nf.json -m BATCH -s ASYNC -e GOAL -eg 0.9934 -et 30h data207 100
-python hpo_runner.py -rc p6div-no_log-rand.json -m BATCH -s ASYNC -e GOAL -eg 0.9934 -et 30h data207 100
-python hpo_runner.py -rc p6div-no_log-nc.json -m BATCH -s ASYNC -e GOAL -eg 0.9934 -et 30h data207 100
-python hpo_runner.py -rc p6div-no_log.json -m BATCH -s ASYNC -e GOAL -eg 0.9934 -et 30h data207 100
+#python hpo_runner.py -rc p6div-no_log-nf.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
+#python hpo_runner.py -rc p6div-no_log-rand.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
+#python hpo_runner.py -rc p6div-no_log-nc.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
+python hpo_runner.py -rc p6div-no_log.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
 # no log + ETR
-python hpo_runner.py -rc p6div-no_log-etr-nc.json -m BATCH -s ASYNC -e GOAL -eg 0.9934 -et 30h data207 100
-python hpo_runner.py -rc p6div-no_log-etr.json -m BATCH -s ASYNC -e GOAL -eg 0.9934 -et 30h data207 100
+python hpo_runner.py -rc p6div-no_log-etr-nc.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
+python hpo_runner.py -rc p6div-no_log-etr.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
 # hybrid log + ETR
-#python hpo_runner.py -rc p6div-etr-nf.json -m BATCH -s ASYNC -e GOAL -eg 0.9934 -et 30h data207 100
-#python hpo_runner.py -rc p6div-etr-rand.json -m BATCH -s ASYNC -e GOAL -eg 0.9934 -et 30h data207 100
-#python hpo_runner.py -rc p6div-etr-nc.json -m BATCH -s ASYNC -e GOAL -eg 0.9934 -et 30h data207 100
-#python hpo_runner.py -rc p6div-etr.json -m BATCH -s ASYNC -e GOAL -eg 0.9934 -et 30h data207 100
+#python hpo_runner.py -rc p6div-etr-nf.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
+#python hpo_runner.py -rc p6div-etr-rand.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
+#python hpo_runner.py -rc p6div-etr-nc.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
+#python hpo_runner.py -rc p6div-etr.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
 
--- xcutes/my_laptop.sh
+++ xcutes/my_laptop.sh
@@ -1,8 +1,8 @@
 # hybrid log + no ETR
-python hpo_runner.py -rc p6div-nf.json -m BATCH -s ASYNC -e GOAL -eg 0.9934 -et 30h data207 100
-python hpo_runner.py -rc p6div-rand.json -m BATCH -s ASYNC -e GOAL -eg 0.9934 -et 30h data207 100
-python hpo_runner.py -rc p6div-nc.json -m BATCH -s ASYNC -e GOAL -eg 0.9934 -et 30h data207 100
-python hpo_runner.py -rc p6div.json -m BATCH -s ASYNC -e GOAL -eg 0.9934 -et 30h data207 100
+#python hpo_runner.py -rc p6div-nf.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
+#python hpo_runner.py -rc p6div-rand.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
+#python hpo_runner.py -rc p6div-nc.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
+python hpo_runner.py -rc p6div.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
 # no log + ETR
-python hpo_runner.py -rc p6div-no_log-etr-nf.json -m BATCH -s ASYNC -e GOAL -eg 0.9934 -et 30h data207 100
-python hpo_runner.py -rc p6div-no_log-etr-rand.json -m BATCH -s ASYNC -e GOAL -eg 0.9934 -et 30h data207 100
+python hpo_runner.py -rc p6div-no_log-etr-nf.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
+python hpo_runner.py -rc p6div-no_log-etr-rand.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
