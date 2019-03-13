002683e180ca9031a6eb128fe85145ec23388369
Hyunghun Cho <hyunghunny@gmail.com>
2019-03-13 23:17:57 +0900

    update one hot test

--- xcutes/970.sh
+++ xcutes/970.sh
@@ -15,11 +15,8 @@
 #python hpo_runner.py -rc p6div-etr-rand.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
 #python hpo_runner.py -rc p6div-etr-nc.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
 #python hpo_runner.py -rc p6div-etr.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
-python hpo_runner.py -rc p6div-one_hot_grid.json -m GP -s EI -et 12h data3 51
+#python hpo_runner.py -rc p6div-one_hot_grid.json -m GP -s EI -et 12h data3 51
 python hpo_runner.py -rc p6div-etr-one_hot_grid.json -m DIV -s SEQ -et 12h data3 51
 python hpo_runner.py -rc p6div-one_hot_grid.json -m GP -s EI -et 12h data20 100
 python hpo_runner.py -rc p6div-etr-one_hot_grid.json -m DIV -s SEQ -et 12h data20 100
 python hpo_runner.py -rc p6div-one_hot_grid.json -m GP -s EI -et 12h data30 100
-python hpo_runner.py -rc p6div-etr-one_hot_grid.json -m DIV -s SEQ -et 12h data30 100
-python hpo_runner.py -rc p6div-one_hot_grid.json -m GP -s EI -et 90h data207 100
-python hpo_runner.py -rc p6div-etr-one_hot_grid.json -m DIV -s SEQ -et 90h data207 100
