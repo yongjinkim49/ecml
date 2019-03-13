1525298d61c8781a5de0951f3d343a013ebffcef
Hyunghun Cho <hyunghunny@gmail.com>
2019-03-13 17:39:47 +0900

    re-test due to miss config

--- xcutes/970.sh
+++ xcutes/970.sh
@@ -15,4 +15,6 @@
 #python hpo_runner.py -rc p6div-etr-rand.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
 #python hpo_runner.py -rc p6div-etr-nc.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
 #python hpo_runner.py -rc p6div-etr.json -m BATCH -s ASYNC -e GOAL -eg 0.9334 -et 30h data207 100
-python hpo_runner.py -rc p6div-etr-one_hot_grid.json -m GP -s EI -et 12h data3 50
+python hpo_runner.py -rc p6div-one_hot_grid.json -m RF -s EI -et 12h data3 50
+python hpo_runner.py -rc p6div-one_hot_grid.json -m GP -s EI -et 12h data3 50
+python hpo_runner.py -rc p6div-etr-one_hot_grid.json -m DIV -s SEQ -et 12h data3 14
